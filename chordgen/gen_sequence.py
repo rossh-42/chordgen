2f58798822bcdbb4141d22768665b15136a72bf1
rossh-42 <58987895+rossh-42@users.noreply.github.com>
2019-12-17 10:34:04 -0600

    Initial check-in for basic files (#1)

---
+++ chordgen/gen_sequence.py
@@ -0,0 +1,14 @@
+import argparse
+from chordgen import ChordMap
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate a series of chord sequences')
+    parser.add_argument('key', type=str, help='key')
+    parser.add_argument('start', type=str, help='name of the chord to start from')
+    parser.add_argument('num', type=int, help='number of chords in each sequence')
+
+    args = parser.parse_args()
+    cm = ChordMap(args.key)
+    for seq in cm.gen_sequence(args.start, args.num):
+        print(seq)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+def get_global(file_name, global_name):
+    """Read global_name from file."""
+    import os
+    globals = {}
+    exec(open(os.path.join(os.path.dirname(__file__), "chordgen", file_name)).read(), globals)
+    return globals[global_name]
+
+
+setup(
+    name=get_global("version.py", "__title__"),
+    version=get_global("version.py", "__version__"),
+    install_requires=['musthe',
+                      'networkx'],
+    description=get_global("version.py", "__description__"),
+    long_description=get_global("version.py", "__description__"),
+    author=get_global("version.py", "__author__"),
+    author_email=get_global("version.py", "__author_email__"),
+    packages=find_packages()
+)
