808f5c17a9e5ba69c99cbf6c2b598eb6c78f3b5a
rossh-42 <58987895+rossh-42@users.noreply.github.com>
2019-12-20 15:54:45 -0600

    A bunch of stuff (#17)

--- mellowchord/__init__.py
+++ mellowchord/__init__.py
@@ -19,4 +19,10 @@ from .mellowchord import KeyedChord  # noqa: F401
 from .mellowchord import chords_types_are_equal  # noqa: F401
 from .mellowchord import ChordParseError  # noqa: F401
 from .mellowchord import MidiFile  # noqa: F401
+from .mellowchord import make_file_name_from_chord_sequence  # noqa: F401
+from .mellowchord import chordgen  # noqa: F401
+from .mellowchord import validate_key  # noqa: F401
+from .mellowchord import validate_start  # noqa: F401
+from .mellowchord import InvalidArgumentError  # noqa: F401
+from .mellowchord import MellowchordError  # noqa: F401
 from .cli import main  # noqa: F401
